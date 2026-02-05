0226a4aac2ba7e5c599b6e9cd3afd50d4bf8436a
Sky Adams <sky_adams@alumni.brown.edu>
2026-02-05 12:51:09 -0800

    Add DB path to sql_execution.py

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,25 +5,7 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
-
-
-# OPTION 1: MIXIN
-# Define a class called `QueryMixin`
-class QueryMixin:
-    
-    # Define a method named `pandas_query`
-    # that receives an sql query as a string
-    # and returns the query's result
-    # as a pandas dataframe
-    #### YOUR CODE HERE
-
-    # Define a method named `query`
-    # that receives an sql_query as a string
-    # and returns the query's result as
-    # a list of tuples. (You will need
-    # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
+db_path = Path('employee_events.db').resolve()
     
 
  
